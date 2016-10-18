5eb12ee52832ffd72e5fe8211abece79db11f367
orstavik <orstavik77@gmail.com>
2016-10-18 11:16:13 +0200

    ivar in 2.0

---
+++ json/images_backup.js
@@ -0,0 +1,66 @@
+//{
+//                            a: {
+//                                name: "a",
+//                                link: "../images/keys/a.png"
+//                            },
+//                            d: {
+//                                name: "d",
+//                                link: "../images/keys/d.png"
+//                            }
+//                            ,
+//                            s: {
+//                                name: "s",
+//                                link: "../images/keys/s.png"
+//                            }
+//                            ,
+//                            f: {
+//                                name: "f",
+//                                link: "../images/keys/f.png"
+//                            }
+//                            ,
+//                            g: {
+//                                name: "g",
+//                                link: "../images/keys/g.png"
+//                            }
+//                            ,
+//                            h: {
+//                                name: "h",
+//                                link: "../images/keys/h.png"
+//                            }
+//                            ,
+//                            j: {
+//                                name: "j",
+//                                link: "../images/keys/j.png"
+//                            }
+//                            ,
+//                            k: {
+//                                name: "k",
+//                                link: "../images/keys/k.png"
+//                            }
+//                            ,
+//                            l: {
+//                                name: "l",
+//                                link: "../images/keys/l.png"
+//                            }
+//                            ,
+//                            bone: {
+//                                name: "bone",
+//                                link: "http://worldartsme.com/images/yellow-triangle-clipart-1.jpg"
+//                            }
+//                            ,
+//                            yell: {
+//                                name: "yell",
+//                                link: "http://vignette3.wikia.nocookie.net/thesimssocial/images/b/b3/Dog-Bone-(item).png/revision/latest?cb=20121122020046"
+//                            }
+//                            ,
+//                            hand: {
+//                                name: "hand",
+//                                link: "https://lh3.google.com/u/0/d/0B5StfgsAnkuDaHJ2OVBrVVZRalk=w1366-h599-k-iv1?v=1476266095410"
+//                            }
+//                            ,
+//                            eye: {
+//                                name: "eye",
+//                                link: "https://lh3.google.com/u/0/d/0B5StfgsAnkuDNk5NQmFTbU5LUmc=w742-h583-k-iv1?v=1476266095410"
+//                            }
+//                        }
+//                    }

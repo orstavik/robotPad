a62dd1e1645170212209a01fe3501839348dd3c7
orstavik <orstavik77@gmail.com>
2017-02-09 11:46:15 +0100

    making scaling percent reflect if the shapes are growing or shrinking. simplifying the droppad-item-marker

--- src/ShapeInfoObject.js
+++ src/ShapeInfoObject.js
@@ -63,24 +63,22 @@ class ShapeInfoObject {
     const c = this.clone();
     //todo here I need to recalculate the different orientations based on c.angle..
     if (direction.indexOf("s") >= 0) {
-      let topDistance = box.top - this.y;
-      c.y -= topDistance * yPercent;
+      let topDistance = this.y-box.top;
+      c.y += topDistance * yPercent;
       c.h *= (1+yPercent);
-    }
-    if (direction.indexOf("n") >= 0) {
-      let bottomDistance = this.y - box.bottom;
+    } else if (direction.indexOf("n") >= 0) {
+      let bottomDistance = box.bottom-this.y;
       c.y -= bottomDistance * yPercent;
-      c.h *= (1-yPercent);
+      c.h *= (1+yPercent);
     }
     if (direction.indexOf("e") >= 0) {
       let leftDistance = this.x - box.left;
       c.x += leftDistance * xPercent;
       c.w *= (1+xPercent);
-    }
-    if (direction.indexOf("w") >= 0) {
+    } else if (direction.indexOf("w") >= 0) {
       let rightDistance = box.right - this.x;
-      c.x += rightDistance * xPercent;
-      c.w *= (1-xPercent);
+      c.x -= rightDistance * xPercent;
+      c.w *= (1+xPercent);
     }
     return c;
   }

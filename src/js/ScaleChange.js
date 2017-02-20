db6ca28285c5634c74b7510038b7de5a1ca39f13
orstavik <orstavik77@gmail.com>
2017-02-20 11:15:25 +0100

    babystep f...ings bug

--- src/js/ScaleChange.js
+++ src/js/ScaleChange.js
@@ -1,13 +1,13 @@
 class ScaleChange {
   constructor(startPoint, center) {
     this.start = startPoint;
-    this.center = center;
-    this.xDistStart = this.start.x - this.center.x;
-    this.yDistStart = this.start.y - this.center.y;
-    this.center.x -= this.xDistStart;
-    this.center.y -= this.yDistStart;
-    this.xDistStart *=2;
-    this.yDistStart *=2;
+    this.xDistStart = this.start.x - center.x;
+    this.yDistStart = this.start.y - center.y;
+    this.center = {};
+    this.center.x = center.x - this.xDistStart;
+    this.center.y = center.y - this.yDistStart;
+    this.xDistStart *= 2;
+    this.yDistStart *= 2;
     this.x = 0;
     this.y = 0;
     this.angle = 0;
@@ -27,8 +27,8 @@ class ScaleChange {
     let whaxy = {w: this.w, h: this.h, angle: 0, x: this.x, y: this.y};
     let xDistPoint = x - this.center.x;
     let yDistPoint = y - this.center.y;
-    whaxy.x = (xDistPoint * (this.w-1));
-    whaxy.y = (yDistPoint * (this.h-1));
+    whaxy.x = (xDistPoint * (this.w - 1));
+    whaxy.y = (yDistPoint * (this.h - 1));
     //todo change .w and .h so that it fits with the angle of the info
     return whaxy;
   }
